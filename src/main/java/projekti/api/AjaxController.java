7d4791f5af4708ae59477e347707a58862ab4f28
Tatu Riihimäki <tatu.riihimaki@gmail.com>
2021-05-13 12:50:47 +0300

    cors testing

--- src/main/java/projekti/api/AjaxController.java
+++ src/main/java/projekti/api/AjaxController.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -35,6 +36,7 @@ import projekti.services.PhotoService;
 import projekti.services.PostService;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api")
 public class AjaxController {
     
